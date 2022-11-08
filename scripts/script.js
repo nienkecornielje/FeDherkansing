027e849100c65c7a74e5483fe4e84e0f22652815
nienkecornielje <79194938+nienkecornielje@users.noreply.github.com>
2022-11-08 17:54:29 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -27,6 +27,9 @@ function eersteImagesButtonKlik() {
 
 var deButton = document.querySelector("header button");
 // menu-button luistert naar klik en voert dan een functie uit
+var deButtonImg = document.querySelector("header button img");
+var deNav = document.querySelector("header nav");
+
 
 deButton.addEventListener("click", toggleMenu);
 // Nieuwe functie toggleMenu
@@ -34,8 +37,10 @@ deButton.addEventListener("click", toggleMenu);
 function toggleMenu() {
   // nav opgeslagen als variabelen
 
-  var deNav = document.querySelector("header nav");
   // nav krijgt class toonMenu
 
   deNav.classList.toggle("toonMenu");
+  // deButtonImg.classList.toggle("swapImg")
+
+  deButton.classList.toggle("swapImg")
 }
